335002b8b8393d42ea470a4839eb37c35a76d365
Denis Fomin <denisfomin2020@gmail.com>
2020-06-05 20:07:01 +0300

    Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 'use strict'
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var TITLES = [
+  'Квартира студия в таунхаусе 40 м2. Рядом с морем.',
+  'Уютная, тихая квартира в центре',
+  'Современная студия для комфортного проживания',
+  'Двухкомнатные апартаменты в самом центре города',
+  'Летний дом',
+  'Комфортабельная квартира',
+  'Уютная квартира в спальном районе',
+  'Тёплый коттедж (посуточно/долгий срок)'];
+var MIN_Y = 130;
+var MAX_Y = 630;
+var MIN_PRICE = 20000;
+var MAX_PRICE = 150000;
+var MAX_GUESTS = 10;
+var MAX_ROOMS = 12;
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECK_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var DESCRIPTIONS = [
+  'Квартира - студия 40 м2. На втором этаже в таунхаусе. Два дивана, обеденная зона, душевая кабина, два шкафа, теплый пол. В квартире есть дополнительный выход на придомовой участок.',
+  '5 минут от аэроэкспресса и от железнодорожного вокзала. 7 минут от исторического центра, доброжелательная хозяйка, милые собака и черная кошка, помощь в ориентации по городу.',
+  'Студия с современным дизайн-ремонтом, со всеми удобствами для проживания. До центра города 10 мин на машине.Рядом три остановки, ближайшая 2 мин пешком.',
+  'Уютная квартира с видом на море, в центре города в шаговой доступности парки, набережные, кафе и городские достопримечательности. Есть всё необходимое для комфортного проживания.',
+  'Большой газон, сирень, вишня, яблони, сливы... Кухня - отдельный вход с веранды, душ и туалет летний, водопровод, электрообогреватель, газ.',
+  'Комфортабельная квартира в центре города , с ремонтом. Имеется все самое необходимое для комфортного проживание.',
+  '2 жилых комнаты, кухня, ванная комната, туалет (удобства в доме). Пол теплый. Отопление – котел.',
+  'Сдаю тёплый коттедж, большая площадь участка, бассейн, баня, рядом находится река «Ока», Тихое и уютное место. Доброжелательные и дружелюбные соседи, в доме есть удобства.'];
 
-var minY = 130;
-var maxY = 630;
-var minPrice = 20000;
-var maxPrice = 150000;
-var maxGuests = 10;
-var maxRooms = 12;
-var types = ['palace', 'flat', 'house', 'bungalo'];
-var checkTimes = ['12:00', '13:00', '14:00'];
-var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-var photos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+var PHOTOS = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
   "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
   "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
+
+var map = document.querySelector('.map');
 var pinTemplate = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
@@ -23,16 +41,16 @@ var pinContainer = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
 
 // Cлучайное число в интервале включительно
-var rnd = function (min, max) {
+var getRandomRangeNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Получаем упорядоченный массив случайной длины случайных элементов исходного массива (для features и photos)
-var getRndElms = function (arr) {
+var getRandomArrayElements = function (arr) {
   var res = [];
-  var len = rnd(1, arr.length);
+  var len = getRandomRangeNumber(1, arr.length);
   while (res.length < len) {
-    var randomIndex = rnd(0, arr.length - 1);
+    var randomIndex = getRandomRangeNumber(0, arr.length - 1);
     if (res.indexOf(randomIndex) === -1) {
       res.push(randomIndex);
     }
@@ -40,48 +58,55 @@ var getRndElms = function (arr) {
   return res.sort(function (a, b) { return a - b }).map(function (a) { return arr[a] });
 }
 
-var createObjects = function (number) {
+var createObject = function (index) {
+  var locationX = getRandomRangeNumber(0, map.offsetWidth);
+  var locationY = getRandomRangeNumber(MIN_Y, MAX_Y);
+  var object = {
+    author: {
+      avatar: 'img/avatars/user0' + (index + 1) + '.png',
+    },
+    location: {
+      x: locationX,
+      y: locationY,
+    },
+    offer: {
+      title: TITLES[index],
+      address: locationX + ', ' + locationY,
+      price: Math.round(getRandomRangeNumber(MIN_PRICE, MAX_PRICE) / 100) * 100,
+      type: TYPES[getRandomRangeNumber(0, TYPES.length - 1)],
+      rooms: getRandomRangeNumber(1, MAX_GUESTS),
+      guests: getRandomRangeNumber(1, MAX_ROOMS),
+      checkin: CHECK_TIMES[getRandomRangeNumber(0, CHECK_TIMES.length - 1)],
+      checkout: CHECK_TIMES[getRandomRangeNumber(0, CHECK_TIMES.length - 1)],
+      features: getRandomArrayElements(FEATURES),
+      description: DESCRIPTIONS[index],
+      photos: getRandomArrayElements(PHOTOS),
+    }
+  }
+  return object;
+}
+
+var createObjectArray = function (number) {
   var objects = [];
   for (var i = 0; i < number; i++) {
-    var locationX = rnd(0, map.offsetWidth);
-    var locationY = rnd(minY, maxY);
-    var object = {
-      author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
-      },
-      location: {
-        x: locationX,
-        y: locationY
-      },
-      offer: {
-        title: 'Объект 0' + (i + 1),
-        address: locationX + ', ' + locationY,
-        price: Math.round(rnd(minPrice, maxPrice) / 100) * 100,
-        type: types[rnd(0, types.length - 1)],
-        rooms: rnd(1, maxGuests),
-        guests: rnd(1, maxRooms),
-        checkin: checkTimes[rnd(0, checkTimes.length - 1)],
-        checkout: checkTimes[rnd(0, checkTimes.length - 1)],
-        features: getRndElms(features),
-        description: 'Описание объекта 0' + (i + 1),
-        photos: getRndElms(photos),
-      }
-    }
-    objects.push(object)
+    objects.push(createObject(i));
   }
-  return objects
+  return objects;
 }
 
 var renderPin = function (object) {
   var pinElement = pinTemplate.cloneNode(true);
+  var pinImage = pinElement.querySelector('img');
   pinElement.style.left = object.location.x + pinElement.offsetWidth / 2 + 'px';
   pinElement.style.top = object.location.y + pinElement.offsetHeight + 'px';
-  pinElement.querySelector('img').src = object.author.avatar;
-  pinElement.querySelector('img').alt = object.offer.title;
-  return pinElement
+  pinImage.src = object.author.avatar;
+  pinImage.alt = object.offer.title;
+  return pinElement;
 }
 
-var objects = createObjects(8);
+map.classList.remove('map--faded');
+var objects = createObjectArray(8);
+console.log(objects)
 for (var i = 0; i < objects.length; i++) {
   fragment.appendChild(renderPin(objects[i]));
 }
